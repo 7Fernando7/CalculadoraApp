5a1706bd21616c4ec1fd7f35c97ec32b837e9ebb
Fernando <info_fotografia@icloud.com>
2024-07-18 17:50:25 +0200

    Funciones y double

--- src/CalculadoraApp.java
+++ src/CalculadoraApp.java
@@ -10,42 +10,14 @@ public class CalculadoraApp {
         Scanner consola = new Scanner(System.in);
         while(true) {
             System.out.println("**** Aplicación Calculadora ****");
-            System.out.println("""
-                    1. Suma
-                    2. Resta
-                    3. Multiplicación
-                    4. División
-                    5. Salir
-                    """);
-            System.out.print("Operación a realizar?");
+            //Mostrar el menu
+            mostrarMenu();
             try {
                 var operacion = Integer.parseInt(consola.nextLine());
                 //Revisar que este dentro de las operaciones mencionadas
                 if (operacion >= 1 && operacion <= 4) {
-                    System.out.print("Proporcione valor operando1: ");
-                    var operando1 = Integer.parseInt(consola.nextLine());
-                    System.out.print("Proporcione valor operando2: ");
-                    var operando2 = Integer.parseInt(consola.nextLine());
-                    int resultado;
-                    switch (operacion) {
-                        case 1 -> {
-                            resultado = operando1 + operando2;
-                            System.out.println("Resultado Suma: " + resultado);
-                        }
-                        case 2 -> {
-                            resultado = operando1 - operando2;
-                            System.out.println("Resultado Resta: " + resultado);
-                        }
-                        case 3 -> {
-                            resultado = operando1 * operando2;
-                            System.out.println("Resultado Multiplicación: " + resultado);
-                        }
-                        case 4 -> {
-                            resultado = operando1 / operando2;
-                            System.out.println("Resultado División: " + resultado);
-                        }
-                        default -> System.out.print("Operación no existe: " + operacion);
-                    }
+                    //Ejecutamos la operacion deseada
+                    ejecutarOperacion(operacion, consola);
                 } else if (operacion == 5) {
                     System.out.println("Hasta pronto...");
                     break;
@@ -60,4 +32,42 @@ public class CalculadoraApp {
             }//Fin catch
         }//Fin while
     }//Fin main
+
+    private static void mostrarMenu(){
+        System.out.println("""
+                    1. Suma
+                    2. Resta
+                    3. Multiplicación
+                    4. División
+                    5. Salir
+                    """);
+        System.out.print("Operación a realizar?");
+    }
+
+    private static void ejecutarOperacion(int operacion, Scanner consola ){
+        System.out.print("Proporcione valor operando1: ");
+        var operando1 = Double.parseDouble(consola.nextLine());
+        System.out.print("Proporcione valor operando2: ");
+        var operando2 = Double.parseDouble(consola.nextLine());
+        double resultado;
+        switch (operacion) {
+            case 1 -> {
+                resultado = operando1 + operando2;
+                System.out.println("Resultado Suma: " + resultado);
+            }
+            case 2 -> {
+                resultado = operando1 - operando2;
+                System.out.println("Resultado Resta: " + resultado);
+            }
+            case 3 -> {
+                resultado = operando1 * operando2;
+                System.out.println("Resultado Multiplicación: " + resultado);
+            }
+            case 4 -> {
+                resultado = operando1 / operando2;
+                System.out.println("Resultado División: " + resultado);
+            }
+            default -> System.out.print("Operación no existe: " + operacion);
+        }
+    }
 }//Fin clase
